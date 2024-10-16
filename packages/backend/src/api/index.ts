aff593741ef838eb0de933454e3ac1c707d31066
Jihan Jasper Al Rashid <j.jasper.a@gmail.com>
2024-10-16 20:36:24 +0600

    fix(backend): correct formatting of Firebase private key

--- packages/backend/src/api/index.ts
+++ packages/backend/src/api/index.ts
@@ -4,7 +4,7 @@ import admin from "firebase-admin";
 const serviceAccount = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
 };
 
 if (!admin.apps.length) {
