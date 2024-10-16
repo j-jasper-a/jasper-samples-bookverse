4b0e64ea5925aff32e11321651cfcd44f524f7a7
Jihan Jasper Al Rashid <j.jasper.a@gmail.com>
2024-10-16 17:53:44 +0600

    refactor(backend): correct paths

---
+++ packages/backend/dist/api/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+app.use(express.json()); // For JSON parsing
+app.get("/", (req, res) => {
+    res.send("Hello from Vercel!");
+});
+app.get("/about", (req, res) => {
+    res.send("About Page");
+});
+export default app;
